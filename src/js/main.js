33141e1d2e2757a18166af01d3263ae083feef58
Sergey Mazeev <sergey.mazeev@gmail.com>
2019-06-13 10:09:39 +0500

    Phone mask for inputs with simple visual validation on input blur

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,28 @@ watchScrollY();
     showModal(e.target); // or showModal('#modal') or showModal('.modal') or showModal(document.querySelector('#modal'))
   })
 });*/
+/****************
+ * Phone mask example
+ ****************/
+
+const maskedPhonesInput = document.querySelectorAll('input[type="tel"]');
+const maskOptions = {
+  mask: '+{7} (000) 000-00-00',
+};
+[...maskedPhonesInput].map((el) => {
+  const maskedInput = IMask(el, maskOptions);
+  el.addEventListener('keydown', () => {
+    if (maskedInput.unmaskedValue === '78') {
+      maskedInput.value = '+7 (';
+    }
+  });
+  // simple validation
+  el.addEventListener('blur', () => {
+    if (maskedInput.unmaskedValue.length === 11) {
+      maskedInput.el.input.classList.add('input_valid')
+    }
+    else {
+      maskedInput.el.input.classList.add('input_invalid')
+    }
+  })
+});
